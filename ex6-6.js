86b0a5e3d415b75f54cad4d6a76e292f90e9e412
zm16555 <zm16555@josai.ac.jp>
2018-10-28 21:54:51 -0400

    message

---
+++ ex6-6.js
@@ -0,0 +1,15 @@
+var who66 = 'world';
+// var come = 'が来た'
+function sayhello66() {
+// var who = 'world';
+  alert('Hello, ' + who66 + '!');
+} 
+
+function taro66() {
+  who66 = '太郎' ;
+} 
+function hanako66() {
+  who66 = '花子';
+}
+function gakita() {alert(who66 + 'が来た');
+}
---
+++ ex6-9.js
@@ -0,0 +1,4 @@
+function txtchange1(){
+document.getElementById('txt1').innerHTML='ナンバーワン！';}
+}
+function imgchange1(){document.getElementById('logo').src='./image/josai2.jpg';}
